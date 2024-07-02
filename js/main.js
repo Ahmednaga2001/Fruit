692cb0c23133feb9b76251137aa315e95614971a
Ahmed Naga <ahmednaga02001@gmail.com>
2024-07-02 05:42:23 +0300

    first upload

--- js/main.js
+++ js/main.js
@@ -13,3 +13,40 @@ let addButton = document.querySelector(".add-product");
 addButton.addEventListener("click", ()=>{
     window.location.href = "/modal.html"
 });
+
+let cards = document.querySelector(".cards");
+const getProducts = JSON.parse(localStorage.getItem("products"))
+console.log(getProducts);
+
+    
+
+const remove = (i)=>{
+    console.log(i);
+    console.log(getProducts.splice(i,1));
+    getProducts.splice(i,1)
+    localStorage.setItem("products" , JSON.stringify(getProducts))
+    window.location.reload()
+    console.log(getProducts);
+}
+
+for(let i = 0 ; i<getProducts.length ; i++){
+    let card =  `<div class="card bg-[#494747ab] mx-auto md:mx-0 max-w-sm md:max-w-lg rounded-md overflow-hidden ">
+              <img src=${getProducts[i].url} class="w-full h-[300px]" alt="" srcset="">
+            <div class="p-2 space-y-2">
+            <h2 class="">${getProducts[i].name}</h2>
+            <p class="">${getProducts[i].desc}</p>
+            <span class="block">${getProducts[i].price}</span>
+            <div class="buttons flex gap-4 ">
+            <div class="quantity border w-full flex justify-evenly p-1">
+                <button><i class="fa-solid fa-plus"></i></button>
+                <span>1</span>
+                <button><i class="fa-solid fa-minus"></i></button>
+            </div>
+            <button class="w-full bg-red-500" onClick=remove(${i})>Remove</button>
+            </div>
+            </div>
+        </div>
+        `;
+cards.innerHTML += card
+
+}
--- js/modal.js
+++ js/modal.js
@@ -5,7 +5,6 @@ let getName = document.querySelector("#name");
 let getPrice = document.querySelector("#price");
 let getDesc = document.querySelector("#desc");
 let add = document.querySelector(".add");
-let cards = document.querySelector(".cards");
 
 
 
@@ -30,26 +29,7 @@ add.addEventListener('click', (e) => {
             arr.push(obj);
             console.log(arr);
 
-            arr.map((el)=>{
-                let card =  `<div class="card bg-[#494747ab] mx-auto md:mx-0 max-w-sm md:max-w-lg ">
-                    <img src=${el.url} alt="" srcset="">
-                    <div class="p-2 space-y-2">
-                    <h2 class="">${el.name}</h2>
-                    <p class="">${el.desc}</p>
-                    <span class="block">${el.price}</span>
-                    <div class="buttons flex gap-4 ">
-                    <div class="quantity border w-full flex justify-evenly p-1">
-                        <button><i class="fa-solid fa-plus"></i></button>
-                        <span>1</span>
-                        <button><i class="fa-solid fa-minus"></i></button>
-                    </div>
-                    <button class="w-full bg-red-500">Remove</button>
-                    </div>
-                    </div>
-                </div>
-                `;
-            cards.appendChild(card)
-            })
+       
 
             localStorage.setItem("products", JSON.stringify(arr));
 
@@ -57,6 +37,7 @@ add.addEventListener('click', (e) => {
             getName.value = "";
             getPrice.value = "";
             getDesc.value = "";
+            window.location.href = "index.html"
         }
     } else {
         alert("دخل البيانات ي نجوميه");
